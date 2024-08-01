813851a7f7c03ff9d2cbf140f2484681f80e438e
Luiz Henrique De Jesus Correia <38606125814@info.local>
2024-08-01 10:53:08 -0300

    s

---
+++ ExerciciosTestes1/Livros.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosTestes1
+{
+    public static class Livros
+    {
+        public static string[] AdicionarLivros(string livro)
+        {
+            string[] livros = { "O Iluminado, Stephen King", "Diario de Anne Frank, Anne Frank", "1984, George Orwell" };
+
+            livros[4] = livro;
+
+            return livros;
+        }
+    }
+}
